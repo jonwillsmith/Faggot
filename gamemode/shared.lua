06f5b6cca6a964695b580e76bbce38f7cc6f5ce3
jonwillsmith <34851424+jonwillsmith@users.noreply.github.com>
2018-01-11 23:37:07 -0700

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,7 +1,10 @@
+DeriveGamemode("sandbox")
+
 GM.Name         = "Propkill Revivified"
 GM.Author       = "Archie and Coke_Is_Awesome" --But credit is due to those gamemodes by Anthr4x, and Geforce.
 
-DEATHMATCH  = team.SetUp(1, "DeathMatch",  Color(255,115,0,255))
-RED         = team.SetUp(2, "Red Team",    Color(255,0,0,255))
-GREEN       = team.SetUp(3, "Blue team",   Color(0,255,0,255))
-BLUE        = team.SetUp(3, "Blue team",   Color(0,0,255,255))
+T_DM          = team.SetUp(1, "DeathMatch",  Color(255,115,0,255))
+T_RED         = team.SetUp(2, "Red Team",    Color(255,0,0,255))
+T_GREEN       = team.SetUp(3, "Blue team",   Color(0,255,0,255))
+T_BLUE        = team.SetUp(3, "Blue team",   Color(0,0,255,255))
+
