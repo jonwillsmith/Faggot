a162f55ffc819d377290b82fe23761a269057264
jonwillsmith <34851424+jonwillsmith@users.noreply.github.com>
2018-01-11 23:15:47 -0700

    Create shared.lua

---
+++ gamemode/shared.lua
@@ -0,0 +1,6 @@
+GM.Name         = "Propkill Revivified"
+GM.Author       = "Archie and Coke_Is_Awesome" --But
+
+TEAM_DEATHMATCH = team.SetUp(1, "DeathMatch", Color(255, 204, 0, 255))
+TEAM_RED        = team.SetUp(2, "Red Team", Color(204, 102, 102, 255))
+TEAM_BLUE       = team.SetUp(3, "Blue team", Color(153, 153, 255, 255))
